10ba2ca1ba1c7c4287e0baba67c75125877edb8a
Bobo4ka <bobo4ka@google.com>
2020-04-16 02:55:12 +0300

    master

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-const Discord = re
-quire('discord.js');
+const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require('./config.json');
-
 bot.login(config.token);
-
 bot.on("ready", () => {
     console.log(`bot start ${bot.user.username}`);
     bot.generateInvite(["ADMINISTRATOR"]).then(link => {
