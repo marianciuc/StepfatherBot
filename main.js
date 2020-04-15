c0f08b342b843dff36d986db69a69dc59e67393c
Bobo4ka <bobo4ka@google.com>
2020-04-16 02:18:11 +0300

    master

--- main.js
+++ main.js
@@ -4,17 +4,9 @@ const config = require('./config.json');
 
 bot.login(config.token);
 
-client.on('message', async message => {
-    // Voice only works in guilds, if the message does not come from a guild,
-    // we ignore it
-    if (!message.guild) return;
-  
-    if (message.content === '/join') {
-      // Only try to join the sender's voice channel if they are in one themselves
-      if (message.member.voice.channel) {
-        const connection = await message.member.voice.channel.join();
-      } else {
-        message.reply('You need to join a voice channel first!');
-      }
-    }
-  });
+bot.on("ready", () => {
+    console.log(`bot start ${bot.user.username}`);
+    bot.generateInvite(["ADMINISTRATOR"]).then(link => {
+        console.log(link);
+    });
+});
