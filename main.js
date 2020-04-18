98c8c38ff40fd03c54e262db876246a03ccbfed7
Bobo4ka <bobo4ka@gmail.com>
2020-04-19 00:00:26 +0300

    master

--- main.js
+++ main.js
@@ -1,42 +1,46 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
-const {
-    prefix,
-    token,
-}=require('./config.json');
+const { prefix, token } = require("./config.json");
 bot.login(token);
 bot.once("ready", () => {
-    console.log(`bot start ${bot.user.username}`);
-    bot.generateInvite(["ADMINISTRATOR"]).then(link => {
-        console.log(link);
-    });
+  console.log(`bot start ${bot.user.username}`);
+  bot.generateInvite(["ADMINISTRATOR"]).then((link) => {
+    console.log(link);
+  });
 });
-bot.on('message', async message => {
-  console.log("Chat log: "+message.content);
+bot.on("message", async (message) => {
+  console.log("Chat log: " + message.content);
   if (message.author.bot) return;
-  if (!message.content.startsWith(prefix))return;
-  if (message.content === prefix+'help') {
-  message.channel.send({embed: {
-    color: 0xFFD63E,
-    author:{
-        name: bot.user.username+" Help list",
-        icon_url: bot.user.avatarURL
-  },
-  fields:[{
-      name: "**__!HELP__**",
-      value: "Show list with bot commands"
-    },
-    {
-        name:"Donate",
-        value: "Don`t forget support us [Donate link](http://google.com)."
-    }
-]}})}
+  if (!message.content.startsWith(prefix)) return;
+  //!help
+  if (message.content === prefix + "help") {
+    message.channel.send({
+      embed: {
+        color: 0xffd63e,
+        author: {
+          name: bot.user.username + " Help list",
+          icon_url: bot.user.avatarURL,
+        },
+        fields: [
+          {
+            name: "**__!HELP__**",
+            value: "Show list with bot commands",
+          },
+          {
+            name: "Donate",
+            value: "Don`t forget support us [Donate link](http://google.com).",
+          },
+        ],
+      },
+    });
+  }
+  //end help list
 });
 
-async function con(mem){
-    if(mem.voice.channel){
-        const connection = await mem.voice.channel.join();
-    }else {
-        system.log("sry");
-    }
+async function MusicOn(userpreference) {
+  if (mem.voice.channel) {
+    const connection = await userpreference.voice.channel.join();
+  } else {
+    system.log("Pleace, join to channel");
+  }
 }
