a87aebf1923690cade501bf744b57d8e1efe2465
Vladimir Maryanchuk <39910684+Hesowam@users.noreply.github.com>
2020-06-17 20:28:31 +0300

    full fix

--- main.js
+++ main.js
@@ -215,7 +215,6 @@ bot.on("voiceStateUpdate", (oldState, newState) => {
                             allow: ['MANAGE_CHANNELS'],
                         },
                     ], 'Needed to change permissions');
-                    log(newState.member.user.id);
                     let interval = setInterval(() => {
                         if (clone.members.size < 1 || !clone) {
                             clone.delete("All users leave.").catch(err => console.error(err.message));
